007c44c2dfddbaad59f2c3a7a2c950199f5cb31d
mingdodev <mingdo.dev@gmail.com>
2025-11-09 22:57:27 +0900

    build: initialize project and add core dependencies
    Include Exposed ORM and Kotest for testing.

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.bandchu"
+version = "0.0.1-SNAPSHOT"
+description = "api"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.exposed:exposed-core:1.0.0-rc-2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:1.0.0-rc-2")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:1.0.0-rc-2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.kotest:kotest-runner-junit5:6.0.4")
+    testImplementation ("io.kotest:kotest-assertions-core:6.0.4")
+    testImplementation ("io.kotest:kotest-property:6.0.4")
+    testImplementation("io.kotest:kotest-extensions-spring:6.0.4")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
