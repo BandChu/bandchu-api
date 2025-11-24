5b09769399e8b813f52d7f08a836207d4555c68e
mingdodev <mingdo.dev@gmail.com>
2025-11-25 01:20:35 +0900

    [BC-24] feat: add arti-profile domain model and exposed table
    #7

--- src/main/kotlin/com/bandchu/api/domain/artist/model/ArtiProfile.kt
+++ src/main/kotlin/com/bandchu/api/domain/artist/model/ArtiProfile.kt
@@ -3,11 +3,12 @@ package com.bandchu.api.domain.artist.model
 import kotlinx.datetime.LocalDateTime
 
 data class ArtiProfile (
-    val artiprofile_id: Int,
-    val artist_name: String,
-    val genre: List<Int>,
+    val id: Long,
+    val artistName: String,
+    val genre: List<String>,
     val description: String,
-    val profile_image_url: String,
-    val created_at: LocalDateTime,
-    val updated_at: LocalDateTime,
+    val profileImageUrl: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+    val memberId: Long,
 )
--- src/main/kotlin/com/bandchu/api/domain/artist/table/ArtiProfileTable.kt
+++ src/main/kotlin/com/bandchu/api/domain/artist/table/ArtiProfileTable.kt
@@ -1,11 +1,18 @@
 package com.bandchu.api.domain.artist.table
 
-import com.bandchu.api.domain.posts.table.PostTable
-import org.jetbrains.exposed.v1.core.Table
+import com.bandchu.api.domain.member.table.MemberTable
+import org.jetbrains.exposed.v1.core.ReferenceOption
+import org.jetbrains.exposed.v1.core.dao.id.LongIdTable
+import org.jetbrains.exposed.v1.datetime.CurrentDateTime
 import org.jetbrains.exposed.v1.datetime.datetime
 
-object ArtiProfileTable : Table("artist_profile") {
+object ArtiProfileTable : LongIdTable("arti_profiles") {
+    val artistName = varchar("artist_name", 20)
+    val genre = array<String>("genre")
+    val description = text("description")
+    val profileImageUrl = varchar("profile_image_url", 255)
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
+    val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)
 
-val id = long("artist_id").autoIncrement()
-override val primaryKey = PrimaryKey(id)
+    val member = reference("members", MemberTable.id, onDelete = ReferenceOption.CASCADE)
 }
