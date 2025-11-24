c8a5895469a91aefe451a12320ce762b355c960b
mingdodev <mingdo.dev@gmail.com>
2025-11-25 03:37:13 +0900

    [BC-24] feat: register arti-profile table in db config
    Remove unused imports.
    
    #7

--- src/main/kotlin/com/bandchu/api/global/config/DatabaseConfig.kt
+++ src/main/kotlin/com/bandchu/api/global/config/DatabaseConfig.kt
@@ -1,22 +1,19 @@
 package com.bandchu.api.global.config
 
 
+import com.bandchu.api.domain.artist.table.ArtiProfileTable
 import com.bandchu.api.domain.posts.table.CommentTable
 import com.bandchu.api.domain.posts.table.MediaTable
 import com.bandchu.api.domain.posts.table.PostTable
 import com.bandchu.api.domain.posts.table.ReportTable
-import com.zaxxer.hikari.HikariDataSource
-import org.jetbrains.exposed.v1.datetime.Minute
-import org.jetbrains.exposed.v1.jdbc.transactions.transaction
-
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
-import org.springframework.context.annotation.Bean
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
-
 import javax.sql.DataSource
+
 @Configuration
 class DatabaseConfig(private val dataSource: DataSource) {
 
@@ -25,7 +22,7 @@ class DatabaseConfig(private val dataSource: DataSource) {
         Database.connect(dataSource)
 
         transaction {
-            SchemaUtils.create(PostTable, CommentTable, MediaTable, ReportTable)
+            SchemaUtils.create(ArtiProfileTable, PostTable, CommentTable, MediaTable, ReportTable)
         }
 
         println(" Exposed + Spring Boot 연결 성공")
