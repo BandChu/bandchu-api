465c9641de11a091c49fbf28b49f85af263f8c95
mingdodev <mingdo.dev@gmail.com>
2025-11-27 00:48:10 +0900

    [BC-24] feat: add concert exposed table
    #7

---
+++ src/main/kotlin/com/bandchu/api/domain/concert/table/ConcertTable.kt
@@ -0,0 +1,18 @@
+package com.bandchu.api.domain.concert.table
+
+import com.bandchu.api.domain.artist.table.ArtiProfileTable
+import org.jetbrains.exposed.v1.core.ReferenceOption
+import org.jetbrains.exposed.v1.core.dao.id.LongIdTable
+import org.jetbrains.exposed.v1.datetime.timestampWithTimeZone
+
+object ConcertTable : LongIdTable("concerts") {
+    val title = varchar("title", 30)
+    val place = varchar("place", 20)
+    val posterImageUrl = varchar("poster_image_url", 255).nullable()
+    val information = text("information").nullable()
+    val bookingUrl = varchar("booking_url", 255).nullable()
+    val bookingSchedule = timestampWithTimeZone("booking_date").nullable()
+    val createdAt = timestampWithTimeZone("created_at")
+
+    val arti_profile = reference("arti_profiles", ArtiProfileTable.id, onDelete = ReferenceOption.CASCADE)
+}
