2e7b60b62b8091be2d1a5748d517b982c65cab33
mingdodev <mingdo.dev@gmail.com>
2025-11-29 04:18:25 +0900

    [BC-29] test: add ConcertFixture
    #22

--- src/test/kotlin/com/bandchu/api/fixture/ArtistFixture.kt
+++ src/test/kotlin/com/bandchu/api/fixture/ArtistFixture.kt
@@ -12,7 +12,7 @@ class ArtistFixture(
 ) {
     /**
      * 외부 환경에 대한 의존을 줄이고, 재사용 가능한 코드를 만들기 위한 테스트 객체 생성 Fixture입니다.
-     * 특정 Member를 위한 Artist Profile을 생성하고, 생성된 ArtistProfile 객체를 반환합니다.
+     * 특정 Member를 위한 Arti-Profile을 생성하고, 생성된 Arti-Profile 객체를 반환합니다.
      */
     fun createArtiProfile(member: Member): ArtiProfile {
         val artiProfile = CreateArtistDetailCommand(
