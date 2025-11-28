b704ffd89a719df2157764c2810d950136d02d91
mingdodev <mingdo.dev@gmail.com>
2025-11-28 15:21:24 +0900

    [BC-24] fix: delete duplicate ProjectConfig
    #7

--- src/test/kotlin/io/kotest/provided/ProjectConfig.kt
+++
@@ -1,9 +0,0 @@
-package io.kotest.provided
-
-import io.kotest.core.config.AbstractProjectConfig
-import io.kotest.extensions.spring.SpringExtension
-
-class ProjectConfig : AbstractProjectConfig() {
-    override val extensions = listOf(SpringExtension())
-}
-
