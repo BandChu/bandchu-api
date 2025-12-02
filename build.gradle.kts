da43b4feec23b549b92c4dd243fc2b5cc2605232
cylin0201 <cylin0201@naver.com>
2025-12-02 14:56:33 +0900

    [BC-26] build: include AWS dependencies and refactor security configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,41 +21,42 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14")
     implementation("org.jetbrains.exposed:exposed-core:1.0.0-rc-2")
     implementation("org.jetbrains.exposed:exposed-jdbc:1.0.0-rc-2")
     implementation("org.jetbrains.exposed:exposed-kotlin-datetime:1.0.0-rc-2")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.4.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.49.0")
+    implementation("org.postgresql:postgresql:42.7.7")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("com.ninja-squad:springmockk:4.0.2")
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+    implementation("software.amazon.awssdk:s3:2.25.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("io.kotest:kotest-runner-junit5:6.0.4")
-    testImplementation ("io.kotest:kotest-assertions-core:6.0.4")
-    testImplementation ("io.kotest:kotest-property:6.0.4")
+    testImplementation("io.kotest:kotest-assertions-core:6.0.4")
+    testImplementation("io.kotest:kotest-property:6.0.4")
     testImplementation("io.kotest:kotest-extensions-spring:6.0.4")
-
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("com.h2database:h2:2.2.224")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    implementation("org.postgresql:postgresql:42.7.1")
-
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
-
-    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
-    implementation("software.amazon.awssdk:s3:2.25.0")
-
-    // Mockk
-    testImplementation("io.mockk:mockk:1.13.13")
 }
 
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
 
