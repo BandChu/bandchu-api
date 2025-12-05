a109bf90942666fb6b855491fb7509a2551f663b
jsshin <jsshin8128@gmail.com>
2025-12-05 18:28:01 +0900

    fix: correct SQL join condition in findLatestSubArtistPost
    - Fix missing FROM-clause error by using SubscriptionTable.artiProfile
    - Remove unused AlbumTable import
    - Fixes 500 error when fetching posts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+        }
+    }
+}
+
 rootProject.name = "api"
