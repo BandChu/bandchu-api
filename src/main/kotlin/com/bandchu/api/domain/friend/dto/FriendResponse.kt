e258394fa478e6cbee5c945f37fe78a3f03fdcfa
Son Hyunbin <147061193+SickofU@users.noreply.github.com>
2026-01-11 22:48:44 +0900

    [BC-37] build: Swagger explanation frined1

--- src/main/kotlin/com/bandchu/api/domain/friend/dto/FriendResponse.kt
+++ src/main/kotlin/com/bandchu/api/domain/friend/dto/FriendResponse.kt
@@ -7,19 +7,19 @@ import org.jetbrains.exposed.v1.core.ResultRow
 import java.time.OffsetDateTime
 
 data class FriendResponse(
-    @get:Schema(description = "공연명 이름", example = "데이먼스이어 단독콘서트")
+    @get:Schema(description = "친구 테이블의 고유 ID", example = "101")
     val id: Long,
 
-    @get:Schema(description = "공연명 이름", example = "데이먼스이어 단독콘서트")
+    @get:Schema(description = "친구 요청 보낸 사람의 member id", example = "11")
     val senderId: Long,
 
-    @get:Schema(description = "공연명 이름", example = "데이먼스이어 단독콘서트")
+    @get:Schema(description = "친구 요청 받은 사람의 member id", example = "22")
     val receiverId: Long,
 
-    @get:Schema(description = "공연명 이름", example = "데이먼스이어 단독콘서트")
+    @get:Schema(description = "친구 요청 상태 PENDING, ACCEPTED 이렇게 두가지 상태가 있습니다.", example = "PENDING, ACCEPTED")
     val status: FriendStatus,
 
-    @get:Schema(description = "공연명 이름", example = "데이먼스이어 단독콘서트")
+    @get:Schema(description = "친구 객체 생성 날짜", example = "2026:01:01 ")
     val createdAt: OffsetDateTime,
 
     val isSentByMe: Boolean // true면 내가 보낸 요청, false면 내가 받은 요청
