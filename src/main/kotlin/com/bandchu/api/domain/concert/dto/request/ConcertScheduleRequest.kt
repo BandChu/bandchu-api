820d29a663b53bc8493dfff369d182c8ca1f2923
Son Hyunbin <147061193+SickofU@users.noreply.github.com>
2026-01-11 22:02:22 +0900

    [BC-37] build: Swagger explanation concert 1

--- src/main/kotlin/com/bandchu/api/domain/concert/dto/request/ConcertScheduleRequest.kt
+++ src/main/kotlin/com/bandchu/api/domain/concert/dto/request/ConcertScheduleRequest.kt
@@ -1,8 +1,12 @@
 package com.bandchu.api.domain.concert.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 
 data class ConcertScheduleRequest(
+
+    @get:Schema(description = "공연 날짜", example = "11월 1일")
+
     @field:NotBlank("공연 날짜를 입력하세요.")
     val date: String
 )
--- src/main/kotlin/com/bandchu/api/domain/concert/dto/response/ConcertDetailResponse.kt
+++ src/main/kotlin/com/bandchu/api/domain/concert/dto/response/ConcertDetailResponse.kt
@@ -1,13 +1,26 @@
 package com.bandchu.api.domain.concert.dto.response
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+
+@Schema(description = "콘서트 세부 사항 응답")
 data class ConcertDetailResponse(
+    @get:Schema(description = "콘서트 고유 ID", example = "101")
     val concertId: Long,
+    @get:Schema(description = "콘서트 제목", example = "펜타포트 2025")
     val title: String,
+    @get:Schema(description = "콘서트 장소", example = "인천 인스파이어 아레나")
     val place: String,
+    @get:Schema(description = "포스터 이미지 url", example = "http://www.image.co,.kr")
     val posterImageUrl: String?,
+    @get:Schema(description = "콘서트 관련 정보", example = "이번 콘서트는 올해 하반기 최대 규모 락 밴드 페스티벌입니다.")
     val information: String?,
+    @get:Schema(description = "콘서트 예약 날짜", example = "예약날짜 2026년 11월 1일 오후 1시 예약")
     val bookingSchedule: String?,
+    @get:Schema(description = "콘서트 예약 url", example = "http://interpark.ck.kr")
     val bookingUrl: String?,
+    @get:Schema(description = "콘서트 공연 스케쥴", example = "11월 5일 11월 6일 11월 7일")
     val performingSchedule: List<ConcertScheduleResponse> = emptyList(),
+    @get:Schema(description = "이 객체 생성 시각", example = "2024:11:00")
     val createdAt: String
 )
