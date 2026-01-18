fdd2d7c3cd949b633829089922b01f940781a0ec
Son Hyunbin <147061193+SickofU@users.noreply.github.com>
2026-01-19 00:05:41 +0900

    [BC-37] build: solve apitest

--- src/test/kotlin/com/bandchu/api/ApiApplicationTests.kt
+++ src/test/kotlin/com/bandchu/api/ApiApplicationTests.kt
@@ -1,6 +1,8 @@
 package com.bandchu.api
 
 import com.bandchu.api.domain.member.service.GoogleOAuthService
+import com.bandchu.api.fixture.AuthFixture
+import com.bandchu.api.fixture.SubscriptionFixture
 import com.bandchu.api.global.config.ConfigController
 import com.bandchu.api.global.config.S3Uploader
 import org.junit.jupiter.api.Test
@@ -16,6 +18,8 @@ class ApiApplicationTests {
     @MockkBean lateinit var s3Uploader: S3Uploader
     @MockkBean lateinit var googleOAuthService: GoogleOAuthService
     @MockkBean lateinit var configController: ConfigController
+    @MockkBean(relaxed = true) lateinit var authFixture: AuthFixture
+    @MockkBean(relaxed = true) lateinit var subscriptionFixture: SubscriptionFixture
     @Test
     fun contextLoads() {
     }
